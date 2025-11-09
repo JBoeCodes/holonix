8faac8edd72dff81a84806a75f6f859ba4983d7f
JBoe <171174488+JBoeCodes@users.noreply.github.com>
2025-11-08 21:22:27 -0500

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "jboedesk NixOS Configuration - Phase 1: Stable Gaming System";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs: {
+    nixosConfigurations.jboedesk = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { 
+        inherit inputs;
+        pkgs-unstable = import nixpkgs-unstable {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+      };
+      modules = [
+        ./hosts/jboedesk/configuration.nix
+      ];
+    };
+  };
+}
